320306d592bf9e77f1dfd7c14d574571aaa2b4ae
crdoug <douglas@technext.com.br>
2024-06-27 19:23:57 -0300

    tests

--- __tests__/model/Conta.test.ts
+++ __tests__/model/Conta.test.ts
@@ -38,6 +38,11 @@ describe("Conta", () => {
     expect(() => { conta.depositar(-5.0); }).toThrow("valor não pode ser igual ou menor que zero");
   });
 
+  test("sacar todo o saldo", async () => {
+    const conta: Conta = criarConta();
+    conta.sacar(5000.0);
+    expect(conta.saldo).toBe(0);
+  });
 
   
 });
--- __tests__/model/Recibo.test.ts
+++ __tests__/model/Recibo.test.ts
@@ -4,5 +4,11 @@ describe("Recibo", () => {
     test("Criar com sucesso", () => {
         const recibo: Recibo = new Recibo();
         expect(recibo.codigo.length).toBe(6);
-    })
+    });
+
+    test("criar recibos diferentes", async () => {
+        const recibo1: Recibo = new Recibo();
+        const recibo2: Recibo = new Recibo();
+        expect(recibo1.codigo).not.toBe(recibo2.codigo);
+    });
 });
